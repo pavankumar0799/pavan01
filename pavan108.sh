f54cb8d7c552dbeb68cdb188c22d9f074d7dc546
pavankumar <rpavankumar7382@gmail.com>
2024-09-21 00:09:30 +0530

    condition

--- pavan108.sh
+++ pavan108.sh
@@ -2,13 +2,13 @@
 
 userid=$(id -u)
 
-if [ $userid -ne 0 ]
+if [ $userid ef 0 ]
 then
     echo "Please run this script with root priveleges"
     exit 1
 fi
 
-dnf list installed git
+dnf install git -y
 
 if [ $? -ne 0 ]
 
